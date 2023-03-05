3f67ec469d3e2109c582fb3d49e442bb60be7e26
puria-asr <poriaasrzad@gmail.com>
2023-03-05 11:44:11 +0330

    lesson-1 compeleted

---
+++ build/js/main.js
@@ -0,0 +1,8 @@
+"use strict";
+let username = 'Pouria Asrzad';
+console.log(username);
+let a = 12;
+let b = 6;
+let c = 2;
+console.log(a / b);
+console.log(a * b);
---
+++ src/main.ts
@@ -0,0 +1,7 @@
+let username ='Pouria Asrzad'
+console.log(username);
+let a =12
+let b= 6
+let c:number=2
+console.log(a/b)
+console.log(a*b)
