559ec8d1771df1aef24696fc8b51326bacfdede8
puria-asr <poriaasrzad@gmail.com>
2023-03-05 12:50:01 +0330

    lesson 3 compeleted

--- build/js/main.js
+++ build/js/main.js
@@ -1,17 +1,61 @@
 "use strict";
-let myName = "Poueia";
-let meaningOfLife;
-let isLoading;
-let album;
-let golam;
-golam = 10;
-meaningOfLife = 22;
-isLoading = true;
-album = true;
-const sum = (a, b) => {
-    return a + b;
+let stringArray = ["one", "two", "three"];
+let guitars = ["Strat", 'Les Paul', 1212];
+let mixedDate = ["ABC", 2023, false];
+// stringArray[0]=15 we can not do this 
+stringArray[0] = "number";
+// stringArray.push(42) we can't do this
+stringArray.push("42");
+guitars[0] = 1948; // it can be ok
+// guitars.unshift(true) we can't fo this
+guitars.unshift('jim');
+// stringArray =guitars error!
+guitars = stringArray; // ++
+let emptyArray = [];
+let bands = [];
+bands.push('dfdf'); //++
+//  bands.push(2)  Error!
+//Tuple
+let myTuple = ['dave', 12, false];
+let mixed = ['dave', 12, false];
+mixed = myTuple; //++
+//  myTuple =mixed  Error!
+myTuple[1] = 42;
+//Objects
+let myObj;
+myObj = [];
+console.log(typeof myObj);
+myObj = bands;
+myObj = {};
+const exampelObj = {
+    p1: 'david',
+    p2: false
 };
-console.log(sum(1, 2));
-let postId;
-let isActive;
-let re = /\w+/g;
+exampelObj.p2 = false;
+exampelObj.p2 = false;
+let evh = {
+    name: 'eddie',
+    active: false,
+    album: [1212, "fgg"]
+};
+let JP = {
+    name: 'Pouria',
+    album: ["1212", "fgg"]
+};
+const greetGuitarist = (guitars) => {
+    if (guitars.name) {
+        return `Hello ${guitars.name.toUpperCase()}!`;
+    }
+    return ' Hello';
+};
+console.log(greetGuitarist(JP));
+//Enums
+var Grade;
+(function (Grade) {
+    Grade[Grade["U"] = 1] = "U";
+    Grade[Grade["D"] = 2] = "D";
+    Grade[Grade["C"] = 3] = "C";
+    Grade[Grade["B"] = 4] = "B";
+    Grade[Grade["A"] = 5] = "A";
+})(Grade || (Grade = {}));
+console.log(Grade.U);
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,104 @@
-let myName:string ="Poueia" 
-let meaningOfLife:number;
-let isLoading:boolean;
-let album:string|boolean;
-let golam :any;
-golam =10
-meaningOfLife=22;
-isLoading=true;
-album=true;
-const sum =(a:number,b:number)=>{
-return a+b;
-}
-console.log(sum(1,2))
-let postId:string|number;
-let isActive :number|boolean;
-let re:RegExp=/\w+/g
+let stringArray =["one","two","three"]
 
+let guitars =["Strat",'Les Paul',1212]
+
+let mixedDate=["ABC",2023,false]
+
+// stringArray[0]=15 we can not do this 
+stringArray[0]= "number"
+// stringArray.push(42) we can't do this
+stringArray.push("42")
+
+guitars[0]=1948 // it can be ok
+
+
+// guitars.unshift(true) we can't fo this
+
+guitars.unshift('jim')
+
+// stringArray =guitars error!
+ guitars =stringArray // ++
+
+ let emptyArray =[]
+
+ let bands:string[]=[]
+
+ bands.push('dfdf')//++
+//  bands.push(2)  Error!
+
+
+//Tuple
+let myTuple:[string,number,boolean]=['dave',12,false]
+
+let mixed =['dave',12,false]
+
+mixed =myTuple //++
+
+//  myTuple =mixed  Error!
+
+myTuple[1]=42
+
+
+
+//Objects
+let myObj :object;
+myObj=[]
+
+console.log(typeof myObj);
+myObj =bands
+myObj={}
+
+
+const exampelObj={
+    p1:'david',
+    p2:false
+}
+
+exampelObj.p2 =false;
+exampelObj.p2 =false;
+
+
+interface Guitarist{
+name?:string,
+active?:boolean,//? mean optionall
+album:(string|number)[]
+}
+
+
+let evh:Guitarist={
+    name:'eddie',
+    active:false,
+    album:[1212,"fgg"]
+}
+
+
+
+let JP:Guitarist={
+    name:'Pouria',
+    album:["1212","fgg"]
+}
+
+const greetGuitarist=(guitars:Guitarist)=>{
+    if(guitars.name){
+        return `Hello ${guitars.name.toUpperCase()}!`
+    }
+
+        return' Hello'
+
+    }
+
+console.log(greetGuitarist(JP));
+
+
+//Enums
+
+
+enum Grade{
+    U=1,
+    D,
+    C,
+    B,
+    A
+}
+
+console.log(Grade.U);
