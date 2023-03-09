c06bd3ed71c7cf870431600427b444838e546ff8
puria-asr <poriaasrzad@gmail.com>
2023-03-09 11:54:50 +0330

    lesson 6 copleted

--- src/main.ts
+++ src/main.ts
@@ -1,43 +1,137 @@
-type One =string
-type Two =string;
-type Three ='hello'
+class Coder{
 
-//convert to more or ledd specific
-let a: One ='hello';
-let b =a as Two //less specific
-let c= a as Three // more specific
+secondLang!:string
+
+    constructor(public readonly name:string,
+       public music :string,
+       private age:number,
+       protected lang:string ='js'){
+        this.name =name
+        this.music =music
+        this.age =age
+        this.lang=lang
+    }
+
+    public getAge(){
+        return `Hello , I'm ${this.age}`
+    }
+}
+
+const Pouria = new Coder('Pouria','asr',22)
+
+console.log(Pouria.getAge());
+// console.log(Pouria.lang);
+
+class WebDev extends Coder{
+    constructor(public computer :string,
+       name:string,
+        music :string,
+        age:number){
+        super(name,music,age)
+this.computer=computer
+    }
 
 
-let d =<One>'word'
-let e =<string | number>'hello'
+    public getLang(){
+        return `I write ${this.lang} `
+    }
+}
+
+const Sara =new WebDev('Mac','Sare','asr',25)
+console.log(Sara.getLang());
+// console.log(Sara.lang);
 
 
-const addOrConcat =(a:number,b:number,c:'add'|'concat'):number|string=>{
-    if(c=='add')return a+b
-    return ''+a+b
+interface Musician{
+name:string,
+instrument:string,
+play(action:string):string
+}
+
+class Guitarist implements Musician{
+    name: string
+    instrument: string
+
+    constructor(name:string,instrument:string){
+        this.name=name
+        this.instrument=instrument
+    }
+
+    play(action: string) {
+        return `${this.name} ${action} the ${this.instrument} `
+    }
+}
+
+const Page =new Guitarist('Pouria','guitar')
+console.log(Page.play('strums'));
+
+
+
+/////////////////////////////
+
+class Peeps{
+    static count :number =0
+    static Getcount():number{
+        return Peeps.count
+    }
+
+
+    public id:number
+
+    constructor(public name :string){
+        this.name=name
+        this.id= ++Peeps.count
+    }
+
+
+
 }
 
 
-
-let myVal : string =addOrConcat(2,2,'concat') as string
-
-
-// Be careful! TS sees no problem - but a string i returned
-let yourVal : number =addOrConcat(2,2,'concat') as number
+const john = new Peeps('john')
+const Steve = new Peeps('Steve')
+const PouriaAsrzad = new Peeps('Pouria')
 
 
+console.log(PouriaAsrzad.id);
+console.log(Steve.id);
+console.log(john.id);
 
-// 10 as string;
-// 10 as unknown as string 
-
-
-//The DOM
-const img = document.querySelector('img')!
-const myImg = document.getElementById('#img') as HTMLImageElement
-
-const YourImg = <HTMLImageElement>document.getElementById('#img') 
+//
+console.log(Peeps.count);
 
 
-img.src
+//////////////////////////'
 
-myImg.src
+
+class Bands{
+    private dataState :string[]
+    constructor (){
+        this.dataState=[]
+    }
+
+
+    public get data():string[]{
+        return this.dataState
+    }
+
+
+    public set data(value:string[]){
+        if(Array.isArray(value)&&value.every(el=>typeof el === 'string')){
+            this.dataState =value
+            return
+        }else throw new Error('Param is not an array of strings')
+    }
+
+}
+
+
+const MyBands =new Bands()
+MyBands.data =['Golam','Led zed']
+console.log(MyBands.data);
+
+MyBands.data =[...MyBands.data,'Hoshang']
+console.log(MyBands.data);
+
+
+// MyBands.data =['Van Halen',222]
